a61e2edcbb822bd70142a3181dc99aee96e1e51f
yuanxiaokun <yuanxiaokun@outlook.com>
2016-05-20 23:05:07 +0800

    第三次提交

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,3 +1,26 @@
-import nt
-a = 1
-print(a)
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 17:24:01 2015
+
+@author: 晓坤
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import numpy as np
+fig = plt. figure()
+ax = fig. gca(projection='3d' )
+X = np.arange(-500, 500, 10)
+Y = np.arange(-500, 500, 10)
+X, Y = np.meshgrid(X, Y)	
+
+Z = (X * np.sin(np.sqrt(np.abs(X))));
+Z = Z + (Y * np.sin(np.sqrt(np.abs(Y))));
+Z = Z + 2*418.9826
+
+surf = ax. plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.cool,
+linewidth=0, antialiased=False)
+ax. set_zlim(0, 1800)
+fig. colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
